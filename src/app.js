f02adf3786d61230bbcd10d19c9ffb21d1f4edf8
mejkyss <124203408+mejkyss@users.noreply.github.com>
2023-04-06 18:46:37 +0200

    Add date & time based on timezones for displayed cities

--- src/app.js
+++ src/app.js
@@ -1,28 +1,21 @@
-function showCityTime(event) {
-  if (event.target.value.length > 0) {
-    if (event.target.value === "paris") {
-      alert(parisTime);
-    }
-    if (event.target.value === "tokyo") {
-      alert(tokyoTime);
-    }
-    if (event.target.value === "sydney") {
-      alert(sydneyTime);
-    }
-  }
-}
+setInterval(function () {
+  //los angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Mo YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
-let citySelect = document.querySelector("#cities");
-citySelect.addEventListener("change", showCityTime);
-
-let parisTime = moment()
-  .tz("Europe/Paris")
-  .format("[It is ]dddd, MMMM D, YYYY h:m A [in Europe/Paris]");
-
-let tokyoTime = moment()
-  .tz("Asia/Tokyo")
-  .format("[It is ]dddd, MMMM D, YYYY h:m A [in Asia/Tokyo]");
-
-let sydneyTime = moment()
-  .tz("Australia/Sydney")
-  .format("[It is ]dddd, MMMM D, YYYY h:m A [in Australia/Sydney]");
+  //prague
+  let pragueElement = document.querySelector("#prague");
+  let pragueDateElement = pragueElement.querySelector(".date");
+  let pragueTimeElement = pragueElement.querySelector(".time");
+  let pragueTime = moment().tz("Europe/prague");
+  pragueDateElement.innerHTML = pragueTime.format("MMMM Mo YYYY");
+  pragueTimeElement.innerHTML = pragueTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
