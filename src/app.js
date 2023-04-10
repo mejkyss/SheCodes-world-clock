28981ca0b7119215473dec9dd8afe83171a1bdad
mejkyss <124203408+mejkyss@users.noreply.github.com>
2023-04-10 14:52:54 +0200

    Add functionality to selected cities, add correct timezone, update UI

--- src/app.js
+++ src/app.js
@@ -1,21 +1,48 @@
 setInterval(function () {
   //los angeles
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Mo YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Mo YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
   //prague
   let pragueElement = document.querySelector("#prague");
-  let pragueDateElement = pragueElement.querySelector(".date");
-  let pragueTimeElement = pragueElement.querySelector(".time");
-  let pragueTime = moment().tz("Europe/prague");
-  pragueDateElement.innerHTML = pragueTime.format("MMMM Mo YYYY");
-  pragueTimeElement.innerHTML = pragueTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  if (pragueElement) {
+    let pragueDateElement = pragueElement.querySelector(".date");
+    let pragueTimeElement = pragueElement.querySelector(".time");
+    let pragueTime = moment().tz("Europe/prague");
+    pragueDateElement.innerHTML = pragueTime.format("MMMM Mo YYYY");
+    pragueTimeElement.innerHTML = pragueTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = event.target.selectedOptions[0].innerHTML;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city"">
+          <div>
+            <h2>${cityName}</h2>
+
+            <div class="date">${cityTime.format("MMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )}<small> ${cityTime.format("A")}</small></div>
+        </div>
+        <div class="city">
+          <div>
+  `;
+}
+
+let citiesSelectElement = document.querySelector("#cities-select");
+citiesSelectElement.addEventListener("change", updateCity);
